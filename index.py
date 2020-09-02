54322c5e4195157537697cf3f8d713b2b8ba6ba6
Rein F <public@reinfernhout.xyz>
2020-09-02 21:18:20 +0200

    rickroll edition

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import discord
+from random import randint
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,6 +17,10 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if "poes" in message.content:
             data = requests.get('https://api.thecatapi.com/v1/images/search?size=small', headers={"X-API-KEY": cat}).json()
+            
+            # 5 percent chance to get rick-rolled
+            if not randint(0,19): data = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
             await message.channel.send(data[0]["url"])
         elif "rick" in message.content:
             await message.channel.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
